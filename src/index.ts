30dd9cdbe71fd48c8288dbfa5a8d5f3d21288005
matomugo60 <matomugo60@gmail.com>
2023-05-20 19:50:51 +0300

    added code for index.html

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Quiz } from "./quiz";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const quizContainer = document.getElementById("quizContainer")!;
+  const questionElement = document.getElementById("question")!;
+  const optionsElement = document.getElementById("options")!;
+  const nextButton = document.getElementById("nextButton")!;
+  const resultContainer = document.getElementById("resultContainer")!;
+  const scoreElement = document.getElementById("score")!;
+
+  const quiz = new Quiz(questionElement, optionsElement, resultContainer, scoreElement);
+
+  nextButton.addEventListener("click", () => {
+    quiz.nextQuestion();
+  });
+
+  quiz.start();
+});
